0731aff240d089fc94873376221a96fd3d4f621c
PockyG <L.Kayasith@gmail.com>
2015-09-03 13:42:05 +1000

    First Unity Commit.
    try pull this.

---
+++ Unity Project/AnomalyPrototype01/Assets/Scripts/Ladder.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var spd = 0.2;
+
+function Start () {
+
+}
+
+function OnTriggerStay (c : Collider) {
+	if (c.gameObject.tag == "Ladder" && Input.GetAxis("Vertical") >= 0.5) {
+		transform.position.y += spd;
+		Debug.Log("Laddering");
+	}
+	Debug.Log("Triggering");
+}
